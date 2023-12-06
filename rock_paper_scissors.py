833cc01488b63e58a83d99692392b7797f65fb4a
Adobyte <adobyte77@gmail.com>
2023-12-06 19:01:40 +0100

    Add a scoring system
    - Also, minor changes to display of play outcome

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,7 +3,11 @@ from random import choice
 
 def play_rock():
 
+    your_score = 0
+    ai_score = 0
+
     while True:
+
         moves_dict = {'r':'rock', 'p':'paper', 's':'scissors'}
         valid_moves_keys = list(moves_dict.keys())
         valid_moves_value = list(moves_dict.values())
@@ -22,10 +26,10 @@ def play_rock():
 
         print("____________")
         if user_input in valid_moves_keys:
-            print(f"You played: {moves_dict[user_input]}")
+            print(f"You played: {moves_dict[user_input]}", end=", ")
             print(f"AI played: {moves_dict[ai_input]}")
         elif user_input in valid_moves_value:
-            print(f"You played: {user_input}")
+            print(f"You played: {user_input}", end=", ")
             print(f"AI played: {moves_dict[ai_input]}")
         print("____________")
 
@@ -34,20 +38,24 @@ def play_rock():
         elif user_input == 'r' and ai_input == 's' or user_input == 'rock' and moves_dict[ai_input]\
                 == 'scissors':
             print("You win!")
+            your_score+=1
         elif user_input == 's' and ai_input == 'p' or user_input == 'scissors' and moves_dict[ai_input]\
                 == 'paper':
             print("You win!")
+            your_score+=1
         elif user_input == 'p' and ai_input == 'r' or user_input == 'paper' and moves_dict[ai_input]\
                 == 'rock':
             print("You win!")
+            your_score+=1
         else:
             print("AI wins!")
+            ai_score+=1
         print("____________")
+        print(f"You:{your_score}  -  AI:{ai_score}")
+        print()
 
 
 if __name__ == '__main__':
     play_rock()
-
-
 
 
