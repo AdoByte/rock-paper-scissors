386cd499ad1329b1cf2fd1159ee9c818e3bd06b4
Adobyte <adobyte77@gmail.com>
2023-12-06 11:37:37 +0100

    Game logic
    - Creates conditions on which the player wins.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,7 +2,6 @@ from random import choice
 
 def play_rock():
 
-
     while True:
         valid_moves = ['rock', 'paper', 'scissors']
         user_input = input("Rock, Paper or Scissors >> ").lower()
@@ -17,13 +16,23 @@ def play_rock():
 
         ai_input = choice(valid_moves)
 
-        print()
+        print("____________")
         print(f"You played: {user_input}")
         print(f"AI played: {ai_input}")
-        print()
+        print("____________")
 
-        
+        if user_input == ai_input:
+            print("It's a tie")
+        elif user_input == 'rock' and ai_input == 'scissors':
+            print("You win!")
+        elif user_input == 'scissors' and ai_input == 'paper':
+            print("You win!")
+        elif user_input == 'paper' and ai_input == 'rock':
+            print("You win!")
+        else:
+            print("AI wins!!!")
 
+        print("____________")
 
 
 
